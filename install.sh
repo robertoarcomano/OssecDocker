7072ac1a7eebd1b37b7406960d9bd175cc2a5c01
Roberto Arcomano <bertolinux@gmail.com>
2020-03-07 16:22:21 +0000

    Working from Github

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+# 0. Constants
+REPO=OssecDocker
+
+# 1. Download Repository
+git clone https://github.com/robertoarcomano/$REPO.git /tmp/$REPO
+cd /tmp/$REPO
+
+# 2. Build Image ossec
 docker build . -t ossec
+
+# 3. Create container
+docker create -p 81:80 --name "ossec" ossec
+
+# 4. Launch container
+docker start ossec
+
+# 5. Delete /tmp/$REPO
+cd
+rm -rf /tmp/$REPO
+
